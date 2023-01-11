5be12f4d7e201198a1e6a9f43f9e1fedfd12cbc5
Leon <ali.rostami33@gmail.com>
2023-01-11 16:12:39 +0330

    sign in & up has been designed.

---
+++ app/src/main/java/com/leon/hamrah_abfa/fragments/SubmitInfoFragment.java
@@ -0,0 +1,36 @@
+package com.leon.hamrah_abfa.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.leon.hamrah_abfa.databinding.FragmentSubmitInfoBinding;
+
+public class SubmitInfoFragment extends Fragment {
+    private FragmentSubmitInfoBinding binding;
+
+    public SubmitInfoFragment() {
+    }
+
+    public static SubmitInfoFragment newInstance() {
+        return new SubmitInfoFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSubmitInfoBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
