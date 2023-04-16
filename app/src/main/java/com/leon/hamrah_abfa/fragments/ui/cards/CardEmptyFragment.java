c7ca20642992f262eb078002ae677690fda7d257
Leon <ali.rostami33gmail.com>
2023-04-16 11:24:56 +0330

    card pager new adapter has been completed...

--- app/src/main/java/com/leon/hamrah_abfa/fragments/ui/cards/CardEmptyFragment.java
+++ app/src/main/java/com/leon/hamrah_abfa/fragments/ui/cards/CardEmptyFragment.java
@@ -1,5 +1,7 @@
 package com.leon.hamrah_abfa.fragments.ui.cards;
 
+import static com.leon.hamrah_abfa.enums.FragmentTags.SUBMIT_INFO;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,9 +10,11 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.leon.hamrah_abfa.R;
 import com.leon.hamrah_abfa.databinding.FragmentCardEmptyBinding;
+import com.leon.hamrah_abfa.fragments.bottom_sheets.SubmitInfoFragment;
 
-public class CardEmptyFragment extends Fragment {
+public class CardEmptyFragment extends Fragment implements View.OnClickListener {
     private FragmentCardEmptyBinding binding;
 
     public CardEmptyFragment() {
@@ -35,5 +39,15 @@ public class CardEmptyFragment extends Fragment {
     }
 
     private void initialize() {
+        binding.imageViewAdd.setOnClickListener(this);
+        binding.textViewEnterAccount.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        final int id = v.getId();
+        if (id == R.id.image_view_add) {
+            SubmitInfoFragment.newInstance().show(getParentFragmentManager(), SUBMIT_INFO.getValue());
+        }
     }
 }
