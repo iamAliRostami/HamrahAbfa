f771b02816734daa63266e953eddaaadd84f8bd4
Leon <ali.rostami33gmail.com>
2023-04-20 21:18:17 +0330

    service form map has been added.

--- app/src/main/java/com/leon/hamrah_abfa/fragments/services/ServiceFormFragment.java
+++ app/src/main/java/com/leon/hamrah_abfa/fragments/services/ServiceFormFragment.java
@@ -3,6 +3,8 @@ package com.leon.hamrah_abfa.fragments.services;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,15 @@ import androidx.fragment.app.Fragment;
 
 import com.leon.hamrah_abfa.R;
 import com.leon.hamrah_abfa.databinding.FragmentServiceFormBinding;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.overlay.MapEventsOverlay;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class ServiceFormFragment extends Fragment implements View.OnClickListener {
     private FragmentServiceFormBinding binding;
@@ -40,8 +51,47 @@ public class ServiceFormFragment extends Fragment implements View.OnClickListene
 
     private void initialize() {
         binding.getRoot().post(() -> binding.mapView.getLayoutParams().height = binding.mapView.getMeasuredWidth());
+        initializeMap();
         binding.buttonSubmit.setOnClickListener(this);
         binding.buttonPrevious.setOnClickListener(this);
+    }
+
+    private void initializeMap() {
+
+        Configuration.getInstance().load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()));
+        binding.mapView.getZoomController().
+                setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT);
+        binding.mapView.setMultiTouchControls(true);
+        final IMapController mapController = binding.mapView.getController();
+        mapController.setZoom(19.0);
+
+//        final GeoPoint startPoint = new GeoPoint(51, 32);
+        final GeoPoint startPoint = new GeoPoint( 32.65462762641145,51.67064483950463);
+        mapController.setCenter(startPoint);
+
+//        if (getLocationTracker(activity).getCurrentLocation() != null) {
+//            final GeoPoint startPoint = new GeoPoint(getLocationTracker(activity).getCurrentLocation().getLatitude(),
+//                    getLocationTracker(activity).getCurrentLocation().getLongitude());
+//            mapController.setCenter(startPoint);
+//        }
+        final MyLocationNewOverlay locationOverlay =
+                new MyLocationNewOverlay(new GpsMyLocationProvider(requireContext()), binding.mapView);
+        locationOverlay.enableMyLocation();
+        binding.mapView.getOverlays().add(locationOverlay);
+        binding.mapView.getOverlays().add(new MapEventsOverlay(new MapEventsReceiver() {
+            @Override
+            public boolean singleTapConfirmedHelper(GeoPoint p) {
+                Log.e("location 1", p.toString());
+                Log.e("location 1", String.valueOf(p.getLatitude()));
+                return false;
+            }
+
+            @Override
+            public boolean longPressHelper(GeoPoint p) {
+                Log.e("location 2", p.toString());
+                return false;
+            }
+        }));
     }
 
     @Override
@@ -59,7 +109,23 @@ public class ServiceFormFragment extends Fragment implements View.OnClickListene
         super.onAttach(context);
         if (context instanceof Activity) serviceActivity = (ICallback) context;
     }
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        binding.mapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
 
+    public void onPause(){
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        binding.mapView.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
     public interface ICallback {
         void submitUserInfo();
 
