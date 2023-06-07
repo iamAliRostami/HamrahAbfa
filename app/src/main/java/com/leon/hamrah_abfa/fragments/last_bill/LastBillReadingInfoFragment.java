73e300c35eab24843e1efa074cc302d6424ea383
Leon <ali.rostami33gmail.com>
2023-06-07 10:26:48 +0330

    last bill separated into multi fragments

---
+++ app/src/main/java/com/leon/hamrah_abfa/fragments/last_bill/LastBillReadingInfoFragment.java
@@ -0,0 +1,66 @@
+package com.leon.hamrah_abfa.fragments.last_bill;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.leon.hamrah_abfa.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link LastBillReadingInfoFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class LastBillReadingInfoFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public LastBillReadingInfoFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment LastBillReadingInfoFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static LastBillReadingInfoFragment newInstance(String param1, String param2) {
+        LastBillReadingInfoFragment fragment = new LastBillReadingInfoFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_last_bill_reading_info, container, false);
+    }
+}
