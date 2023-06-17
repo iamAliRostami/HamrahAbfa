5abba3e29eb244d249144f73bd10ff72edcfaec9
Leon <ali.rostami33gmail.com>
2023-06-17 18:30:00 +0330

    some lists has changed a little.

--- app/src/main/java/com/leon/hamrah_abfa/adapters/recycler_view/ActiveSessionAdapter.java
+++ app/src/main/java/com/leon/hamrah_abfa/adapters/recycler_view/ActiveSessionAdapter.java
@@ -1,6 +1,7 @@
 package com.leon.hamrah_abfa.adapters.recycler_view;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
