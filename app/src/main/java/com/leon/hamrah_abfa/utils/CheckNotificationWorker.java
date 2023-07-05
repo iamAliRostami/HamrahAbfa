446e3ceff91e3c0727cf14cc88c73c419544d808
Leon <ali.rostami33gmail.com>
2023-07-05 14:17:01 +0330

    work library completely has been added.

--- app/src/main/java/com/leon/hamrah_abfa/utils/CheckNotificationWorker.java
+++ app/src/main/java/com/leon/hamrah_abfa/utils/CheckNotificationWorker.java
@@ -1,4 +1,19 @@
 package com.leon.hamrah_abfa.utils;
 
-public class CheckNotificationWorker{
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class CheckNotificationWorker extends Worker {
+    public CheckNotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        return null;
+    }
 }
